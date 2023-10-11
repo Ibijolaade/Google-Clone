f069b8e2ef6993a98094a739f52bb4f6f6f67e2c
Ibijolaade <134616411+Ibijolaade@users.noreply.github.com>
2023-10-11 15:14:47 +0100

    first commit

---
+++ Into To Scrimba/My Profile/index.js
@@ -0,0 +1,13 @@
+// Replace the arguments below according to your preference.
+
+// space, scary, military,, romantic, cowboy, fantasy, superhero
+favouriteMovieGenre("cowboy")
+
+// watermelon, tomato, banana, orange, avocado, blueberry
+favouriteFruit("banana")
+
+// light, dark
+favouriteMode("light")
+
+// sharp, soft, round
+favouriteEdgeStyle("round")
